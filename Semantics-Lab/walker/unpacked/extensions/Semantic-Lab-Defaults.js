efe5541164ac7dd732bbce5990f215d2ba76f772
Peter Krautzberger <p.krautzberger@gmail.com>
2015-10-30 18:07:44 +0100

    add walker demo

---
+++ Semantics-Lab/walker/unpacked/extensions/Semantic-Lab-Defaults.js
@@ -0,0 +1,10 @@
+//
+//  The default options
+//
+Lab.defaults.collapse = true;
+Lab.defaults.overflow = false;
+Lab.defaults.highlight = "none";  // "none", "hover", or "flame"
+Lab.defaults.width = 100;         // a percentage from 10 to 100
+Lab.defaults.background = "blue";
+Lab.defaults.foreground = "black";
+Lab.defaults.renderer = "HTML-CSS";
